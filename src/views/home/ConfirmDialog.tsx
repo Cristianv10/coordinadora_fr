e8ff42a246ca338a195b5d5e4272a771fb96b4a7
Cristian Vargas <131102267+Cristianv10@users.noreply.github.com>
2024-03-24 20:49:51 -0400

    Modal to confirm my suscriptions to the events

---
+++ src/views/home/ConfirmDialog.tsx
@@ -0,0 +1,35 @@
+import { Dialog } from "@material-ui/core";
+import { FC } from "react";
+
+interface ConfirmationDialogProps {
+  open: boolean;
+  onClose: () => void;
+  onConfirm: () => void;
+}
+
+const ConfirmationDialog: FC<ConfirmationDialogProps> = ({ open, onClose, onConfirm }) => {
+  return (
+    <Dialog open={open} onClose={onClose} PaperProps={{
+      style: {
+        backgroundColor: '#f5f5f5', 
+        boxShadow: 'none',  
+        borderRadius: '15px', 
+        padding: '20px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: '300px',
+      },
+    }}>
+      <div style={{textAlign: 'center'}}>
+        <h2>Confirmar cambios</h2>
+        <div style={{display: 'grid', gridTemplateColumns: '1fr 1fr', gap: '10px'}}>
+          <button className="event-button" onClick={onConfirm}>Confirm</button>
+          <button className="event-button" onClick={onClose}>Cancel</button>
+        </div>
+      </div>
+    </Dialog>
+  );
+}
+
+export default ConfirmationDialog;
